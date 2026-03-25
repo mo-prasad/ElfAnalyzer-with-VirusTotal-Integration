faae5013ee07240718678173c5c556366f95313a
courtneylewis95-ai <courtneylewis95@gmail.com>
2026-03-25 19:59:12 -0400

    Update elfanalyzer.py

--- elfanalyzer.py
+++ elfanalyzer.py
@@ -18,7 +18,7 @@ from elftools.common.exceptions import ELFError
 
 # Optional assembly analysis (requires capstone)
 try:
-    from asmanalyzer import ASMAnalyzer, capstone_available
+    from asmanalyzer import ASMAnalyzer, capstone_availablec
     _ASM_AVAILABLE = capstone_available()
 except ImportError:
     _ASM_AVAILABLE = False
